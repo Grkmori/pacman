b45ba7de1ea783c7e21d5572ab3546ff4d2ec905
Grkmori <55816865+Grkmori@users.noreply.github.com>
2025-05-22 11:25:23 -0300

    Add files via upload

---
+++ inc/FileManager.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+
+int LoadHighScore();
+void SaveHighScore(int _hs);
+void LoadMapBoard(std::vector<std::string>& _stringVector);
---
+++ inc/Utils.hpp
@@ -0,0 +1,10 @@
+
+#pragma once
+
+#include <string>
+
+
+// STRING UTILS
+inline std::string _indent(int _spaces) {
+    return std::string(_spaces, ' ');
+}
