d2b7f08b7167afb9a97a4fd69e56f13c5b5c9b63
Grkmori <55816865+Grkmori@users.noreply.github.com>
2025-05-22 13:24:50 -0300

    Add files via upload

---
+++ src/FileManager.cpp
@@ -0,0 +1,81 @@
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <exception>
+
+#include "Utils.hpp"
+#include "game/GameSettings.hpp"
+
+
+int LoadHighScore()
+{
+	std::string line;
+	std::ifstream infile;
+	std::stringstream ss;
+	int hs = 0;
+
+	try {
+		infile.open(HIGH_SCORE_PATH);
+		if (!infile.is_open()) {
+			throw std::runtime_error("Failed to open HighScore.txt");
+			std::cerr << "# HighScore.txt has not been oppened."<< std::endl;
+		}
+	} catch (const std::exception& e) {
+		std::cerr << "# Error: " << e.what() << std::endl;
+	}
+
+	getline(infile, line);
+	ss << line;
+	ss >> hs;
+
+	std::cout << _indent(3) << "+ highScore has been loaded." << std::endl;
+	infile.close();
+	return hs;
+}
+
+void SaveHighScore(int _hs)
+{
+	std::ofstream outfile;
+
+	try {
+		outfile.open(HIGH_SCORE_PATH);
+		if (!outfile.is_open()) {
+			throw std::runtime_error("Failed to open HighScore.txt");
+			std::cerr << "# HighScore.txt has not been oppened."<< std::endl;
+		}
+	} catch (const std::exception& e) {
+		std::cerr << "# Error: " << e.what() << std::endl;
+	}
+
+	outfile << _hs;
+
+	std::cout << _indent(3) << "+ highScore has been saved." << std::endl;
+	outfile.close();
+}
+
+void LoadMapBoard(std::vector<std::string>& _stringVector)
+{
+	std::string line;
+	std::ifstream infile;
+	
+	try {
+		infile.open(MAP_BOARD_PATH);
+		if (!infile.is_open()) {
+			throw std::runtime_error("Failed to open Map.txt");
+			std::cerr << "# Map.txt has not been oppened."<< std::endl;
+		}
+	} catch (const std::exception& e) {
+		std::cerr << "# Error: " << e.what() << std::endl;
+	}
+
+	while (getline(infile, line)) {
+		_stringVector.push_back(line);
+		std::cout << line << std::endl;
+	}
+
+	std::cout << _indent(3) << "+ mapBoard has been loaded." << std::endl;
+	infile.close();
+}
